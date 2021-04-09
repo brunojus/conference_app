0258d899c4dd60d7a9c19fdfe559fb451b24d107
Bruno Praciano <bruno.justino-garcia-praciano@efs-auto.com>
2021-04-09 12:01:24 +0200

    Adding the search for id

--- src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
+++ src/main/java/com/pluralsight/conferencedemo/controllers/SpeakersController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/sessions")
+@RequestMapping("/api/v1/speakers")
 public class SpeakersController {
     @Autowired
     private SpeakerRepository speakerRepository;
